d45d7a25f0725fc75894a851d5047f4693b86aa7
kcbobbe <kcbobbe@gmail.com>
2020-02-22 21:58:40 -0500

    updated ui with dynamic boxes. On click the response box appears.

--- public/script.js
+++ public/script.js
@@ -37,24 +37,68 @@ var messages = [
 
 function createMessageTable() {
   for (var i = 0; i < messages.length; i++){
-    var newMessageRow = $("<tr>");
-    newMessageRow.attr("data-id", i)
-    var newMessagePriority = $("<th>").text(i);
-    var newMessageContent = $("<td>").text(messages[i].Text)
-    var newPhoneNumber = $("<td>").text(messages[i].from)
-    newMessageRow.append(newMessagePriority, newMessageContent, newPhoneNumber)
-    $("#unreadContainer").append(newMessageRow);
+    // var newMessageRow = $("<tr>");
+    // newMessageRow.attr("data-id", i)
+    // var newMessagePriority = $("<th>").text(i);
+    // var newMessageContent = $("<td>").text(messages[i].Text)
+    // var newPhoneNumber = $("<td>").text(messages[i].from)
+    // newMessageRow.append(newMessagePriority, newMessageContent, newPhoneNumber)
+    // $("#unreadContainer").append(newMessageRow);
+    var newMessageBox = $("<div>").addClass("box");
+    var newMessageArticle = $("<article>").addClass("media");
+
+    var newMediaContentContainer =$("<div>").addClass("media-content");
+    var newMediaContent = $("<div>").addClass("content");
+
+
+    var messageContainer = $("<p>")
+    var newNumber = $("<span>").text(messages[i].from);
+    var newTime = $("<span>").text(messages[i].time);
+    var newbr = $("<br>");
+    var newMessage = $("<span>").text(messages[i].Text)
+
+    var newTagContainer = $("<nav>").addClass("level");
+    newTagContainer = $("<nav>").addClass("is-mobile");
+    var newLevelItem = $("<div>").addClass("level-left");
+
+    var newButton = $("<button>").addClass("button").text("Respond");
+    newButton.attr("data-id", i);
+    newButton.addClass("respondButton")
+
+    var newInputDiv = $("<div>").attr("id", "response" + i);
+    newInputDiv.css("display","none")
+    newInputDiv.addClass("responseDiv")
+    var newInput = $("<input>").addClass("input is-fullwidth");
+    newInput.attr("type","text");
+    newInput.attr("placeholder", "Type Your Response")
+
+    newInputDiv.append(newInput)
+
+    newTagContainer.append(newLevelItem);
+    messageContainer.append(newNumber, newTime, newbr, newMessage)
+    newMediaContent.append(messageContainer)
+    newMediaContentContainer.append(newMediaContent, newTagContainer),
+    newMessageArticle.append(newMediaContentContainer)
+    newMessageBox.append(newMessageArticle)
+    newMessageBox.append(newButton , newInputDiv)
+
+    $("#unreadContainer").append(newMessageBox)
   }
 }
 createMessageTable();
 
-$("#unreadContainer").on('click', function(e){
+$(".respondButton").on('click', function(e){
   e.preventDefault();
-  $("#response-container").css("display","");
-  $("#unreadContainer").children().removeClass("active-message");
-  var parentElement = ($(e.target).parent());
-  parentElement.attr("class", "active-message");
-  console.log(parentElement.attr("data-id"))
+  $(".responseDiv").css("display","none")
+  console.log($(this).attr("data-id"))
+  var dataid= $(this).attr("data-id")
+  $("#response" + dataid).css("display","");
+  // $("#unreadContainer").children().removeClass("active-message");
+  // var parentElement = ($(e.target).parent());
+  // console.log($(e.target).parent());
+
+  // parentElement.addClass("active-message");
+  // console.log(parentElement.attr("data-id"))
 })
 
 // clicking the tabs
